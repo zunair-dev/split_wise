bee14bcbe0e81b1edd8253cc6520aa19d144f2ff
Muhammad Zunair <zunair.dev@gmail.com>
2022-09-22 15:34:16 -0700

    introduced status field in trasactions

--- db/migrate/20220920213739_create_transactions.rb
+++ db/migrate/20220920213739_create_transactions.rb
@@ -3,6 +3,7 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
     create_table :transactions do |t|
       t.decimal :amount
       t.integer :currency, default: 0, null: false
+      t.integer :status, default: 0
 
       t.timestamps
     end
