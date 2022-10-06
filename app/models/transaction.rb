ad234fde81ecf75df1164ba3cceafccf85aab67d
Muhammad Zunair <zunair.dev@gmail.com>
2022-10-06 12:55:54 -0700

    added callback

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,17 @@ class Transaction < ApplicationRecord
   CURRENCY = %w[PK USD INR]
 
   validates_presence_of :amount
+  
+  before_action :validate_transaction
 
   enum status: {
     in_progress: 0
     failed:      1
     completed:   2
   }
+
+  private
+
+  def validate_transaction
+  end
 end
